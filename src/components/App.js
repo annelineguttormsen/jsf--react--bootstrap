3c02b2600f22ccd78bf7375aa50d12adf3d16220
Anneline <anneline.guttormsen@hotmail.no>
2020-03-13 14:09:32 +0100

    Finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import Nav from "react-bootstrap/Nav";
+import NavBar from "react-bootstrap/NavBar";
 
 export default function App() {
     return (
         <div className="App">
-            <nav>
-                <a href="/">Home</a>
-                <a href="/section1/">Section 1</a>
-                <a href="/section2/">Section 2</a>
-            </nav>
+            <NavBar bg="primary" variant="dark" expand="lg">
+                <NavBar.Brand href="/">Website Name</NavBar.Brand>
+                <Nav>
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/section1/">Section 1</Nav.Link>
+                    <Nav.Link href="/section2/">Section 2</Nav.Link>
+                </Nav>
+            </NavBar>
         </div>
     );
 }
